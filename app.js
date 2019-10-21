b0fabd1c0c304259fc3d0de516fc5b5d09b77e81
Kolya458 <kononovnickolay@yahoo.com>
2019-10-21 23:38:18 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res, next) => {
+    res.send('now then');
+    next();
+})
