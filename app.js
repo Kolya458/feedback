77fafc87cae0cd9607ea3d1d2c350f906c5ef868
Maxim <Maxim@MacBook-Pro-Maxim.local>
2019-10-27 21:18:42 +0300

    add dependecies

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const lowdb = require('lowdb');
+const bodyParser = require('body-parser')
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
